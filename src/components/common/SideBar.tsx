2872e38977d7843cb938fea6dfa136976c262750
masaki.nakano <bdyht515@yahoo.co.jp>
2025-04-27 11:43:54 +0900

    interfaceを利用

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,7 +1,50 @@
-import { Box, Drawer } from '@mui/material'
+import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, Toolbar } from '@mui/material'
 import React from 'react'
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import ListItemText from '@mui/material/ListItemText';
 
-const SideBar = ({drawerWidth, mobileOpen, handleDrawerToggle}:{drawerWidth: number, mobileOpen: boolean, handleDrawerToggle: () => void}) => {
+
+interface SideBarProps {
+  drawerWidth: number,
+  mobileOpen: boolean,
+  handleDrawerClose: () => void,
+  handleDrawerTransitionEnd: () => void,
+}
+
+const SideBar = ({drawerWidth, mobileOpen, handleDrawerClose, handleDrawerTransitionEnd}: SideBarProps) => {
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
       <Box
         component="nav"

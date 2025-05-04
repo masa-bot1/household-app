e03b9961c3e63238ff4b540ec2a2fb100a338e90
masaki.nakano <bdyht515@yahoo.co.jp>
2025-05-05 08:57:09 +0900

    今月の取引データのみ取得

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import { CssBaseline } from '@mui/material';
 import { Transaction } from './types';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './firebase';
+import { format } from 'date-fns';
+import { formatMonth } from './utils/formatting';
 
 function App() {
 
@@ -19,7 +21,9 @@ function App() {
     return typeof err === 'object' && err !== null && 'code' in err
   }
 
-  const[transactions, setTransactions] = useState<Transaction[]>([]);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  console.log(currentMonth);
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -50,13 +54,19 @@ function App() {
     fetchTransactions();
   }, []);
 
+  const monthlyTransactions = transactions.filter((transaction) => {
+    return transaction.date.startsWith(formatMonth(currentMonth));
+  });
+
+  console.log(monthlyTransactions);
+
   return (
     <ThemeProvider theme={theme}>
     <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" element={<AppLayout />}>
-            <Route index element={<Home />}/>
+            <Route index element={<Home monthlyTransactions={monthlyTransactions} />}/>
             <Route path="/report" element={<Report />}/>
             <Route path="*" element={<NoMatch />}/>
           </Route>
--- src/components/MonthlySummary.tsx
+++ src/components/MonthlySummary.tsx
@@ -3,8 +3,14 @@ import React from 'react'
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import { Transaction } from '../types';
 
-const MonthlySummary = () => {
+interface MonthlySummaryProps {
+  monthlyTransactions: Transaction[]
+}
+
+const MonthlySummary = ({monthlyTransactions}: MonthlySummaryProps) => {
+  console.log(monthlyTransactions);
   return (
     <Grid container spacing={{ xs: 1, sm: 2 }} mb={2}>
       {/* 収入 */}

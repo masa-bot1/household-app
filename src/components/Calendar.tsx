c528d5b929ba17ce2e436a77102a959232b48c03
masaki.nakano <bdyht515@yahoo.co.jp>
2025-05-06 08:43:04 +0900

    カレンダーの見た目作成

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,43 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import jaLocale from '@fullcalendar/core/locales/ja'
+import "../calendar.css"
+import { EventContentArg } from '@fullcalendar/core'
 
 const Calendar = () => {
+  const events = [
+    { title: 'Meeting', start: new Date() },
+    { title: 'asd', start: "2025-05-01", income: 300, expense: 200, balance: 100 }
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className='money'id='event-income'>
+          {eventInfo.event.extendedProps.income}
+        </div>
+
+        <div className='money'id='event-expense'>
+          {eventInfo.event.extendedProps.expense}
+        </div>
+
+        <div className='money'id='event-balance'>
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>
-      Calendar
-    </div>
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 

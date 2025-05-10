35bdfda52b8346dfd2149d02a1f0d46afec12eec
masaki.nakano <bdyht515@yahoo.co.jp>
2025-05-10 11:54:03 +0900

    カテゴリアイコンを表示

--- src/components/TransactionMenu.tsx
+++ src/components/TransactionMenu.tsx
@@ -19,6 +19,7 @@ import FastfoodIcon from "@mui/icons-material/Fastfood";
 import DailySummary from "./DailySummary";
 import { Transaction } from "../types";
 import { formatCurrency } from "../utils/formatting";
+import IconComponents from "./common/IconComponents";
 
 interface TransactionMenuProps {
   dailyTransactions: Transaction[];
@@ -72,8 +73,10 @@ const TransactionMenu = ({dailyTransactions, currentDay}: TransactionMenuProps) 
                   <Card
                     sx={{
                       width: "100%",
-                      backgroundColor: (theme) =>
-                        theme.palette.expenseColor.light,
+                      backgroundColor:
+                        transaction.type === "income"
+                          ? (theme) => theme.palette.incomeColor.light
+                          : (theme) => theme.palette.expenseColor.light,
                     }}
                   >
                     <CardActionArea>
@@ -86,7 +89,7 @@ const TransactionMenu = ({dailyTransactions, currentDay}: TransactionMenuProps) 
                         >
                           <Grid size={{ xs: 1}}>
                             {/* icon */}
-                            <FastfoodIcon />
+                            {IconComponents[transaction.category]}
                           </Grid>
                           <Grid  size={2.5}>
                             <Typography

b281a3db6932a25b708e63ff3f61de3e5616dfc3
masaki.nakano <bdyht515@yahoo.co.jp>
2025-06-18 09:10:28 +0900

    仮データで棒グラフを表示

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,11 +1,64 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const BarChart = () => {
-  return (
-    <div>
-      BarChart
-    </div>
-  )
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const labels = [
+    '2024-01-10',
+    '2024-01-11',
+    '2024-01-12',
+    '2024-01-13',
+    '2024-01-14',
+    '2024-01-15',
+    '2024-01-16'
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '支出',
+        data: [100, 200, 300, 400, 500, 600, 700],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: '収入',
+        data: [900, 800, 700, 600, 500, 400, 300],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
 }
 
 export default BarChart

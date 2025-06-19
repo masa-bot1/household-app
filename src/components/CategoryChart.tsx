7f8b4cba4d44b76dc29478f300a6157cee841bc5
masaki.nakano <bdyht515@yahoo.co.jp>
2025-06-20 08:58:37 +0900

    収支タイプを切り替えるセレクト要素を作成

--- src/components/CategoryChart.tsx
+++ src/components/CategoryChart.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Pie } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Box, MenuItem, TextField } from '@mui/material';
+import { TransactionType } from '../types';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const CategoryChart = () => {
+  const [selectedType, setSelectedType] = useState<TransactionType>("expense");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setSelectedType(e.target.value as TransactionType);
+  }
 
   const data = {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
@@ -34,7 +41,19 @@ const CategoryChart = () => {
   };
 
   return (
-    <Pie data={data} />
+    <Box>
+      <TextField
+        label="収支の種類"
+        select
+        fullWidth
+        value={selectedType}
+        onChange={handleChange}
+      >
+        <MenuItem value={"income"}>収入</MenuItem>
+        <MenuItem value={"expense"}>支出</MenuItem>
+      </TextField>
+      <Pie data={data} />
+    </Box>
   )
 }
 
